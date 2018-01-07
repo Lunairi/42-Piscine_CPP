a8c970314bfae90f5669c728607f50db0bcda4ab
Lunairi <mizukori250@gmail.com>
2018-01-06 16:19:36 -0800

    Finished this, had to help with hackathon and numpy project

--- d01/ex08/Human.cpp
+++ d01/ex08/Human.cpp
@@ -12,14 +12,80 @@
 
 #include <iostream>
 #include <string>
+#include <ctime>
 #include "Human.hpp"
 
-Human::Human(void) 
+void Human::meleeAttack(std::string const & target)
 {
-	return;
+	srand(time(0));
+	int i = rand();
+
+	if ((i % 4 == 0))
+		std::cout << "You use your mythical japanese blade Kusanagi to strike down " << target << std::endl;
+	else if ((i % 4 == 1))
+		std::cout << "You use the legendary Durendal of Charlemagne to strike down " << target << std::endl;
+	else if ((i % 4 == 2))
+		std::cout << "You use the norse mythological Claíomh Solais to strike down " << target << std::endl;
+	else
+		std::cout << "You use the legendary Excalibur of King Author to strike down " << target << std::endl;
 }
 
-Human::~Human(void) 
+void Human::rangedAttack(std::string const & target)
 {
+	srand(time(0));
+	int i = rand();
+
+	if ((i % 4 == 0))
+		std::cout << "You use the legenday Gandiva of Brahma & Arjuna to shoot down " << target << std::endl;
+	else if ((i % 4 == 1))
+		std::cout << "You use the legendary Sharanga of Vishnu to shoot down " << target << std::endl;
+	else if ((i % 4 == 2))
+		std::cout << "You use the norse mythological Ichaival of Odin to shoot down " << target << std::endl;
+	else
+		std::cout << "You use the mythical korean bow Gakgung to shoot down " << target << std::endl;
+}
+
+void Human::intimidatingShout(std::string const & target)
+{
+	srand(time(0));
+	int i = rand();
+
+	if ((i % 4 == 0))
+		std::cout << "You shout 'FUS RO DAH!!!' at " << target << std::endl;
+	else if ((i % 4 == 1))
+		std::cout << "You begin shout singing 'Rick Astley Never Gonna Give You Up' at " << target << std::endl;
+	else if ((i % 4 == 2))
+		std::cout << "You begin shouting and beatboxing sick beats at " << target << std::endl;
+	else
+		std::cout << "You begin shouting and rapping 'Eminem RapGod' five times in a row without breathing at " << target << std::endl;
+}
+
+void Human::action(std::string const & action_name, std::string const & target)
+{
+	int i = 0;
+
+	void (Human::*ptr[])(std::string const &) =
+	{
+		&Human::meleeAttack,
+		&Human::rangedAttack,
+		&Human::intimidatingShout 
+	};
+
+	std::string commands[] =
+	{
+		"meleeAttack",
+		"rangedAttack",
+		"intimidatingShout"
+	};
+
+	while (i < 3)
+	{
+		if (commands[i] == action_name)
+		{
+			(this->*ptr[i])(target);
+			i = 4;
+		}
+		i++;
+	}
 	return;
 }
--- d01/ex08/main.cpp
+++ d01/ex08/main.cpp
@@ -13,3 +13,51 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include "Human.hpp"
+
+int	main(int ac, char **av)
+{
+	Human 		attack;
+	std::string wep;
+	std::string	target;
+
+	if (ac != 3)
+	{
+		std::cout << "You must provide an attack and your target." << std::endl <<
+			"Valid attacks (case sensitive): meleeAttack, rangedAttack, intimidatingShoot" << std::endl << std::endl <<
+			"Usage: ./a.out [Valid Attack] [Your Target/Victim]" << std::endl;
+		return (0);
+	}
+	wep = av[1];
+	target = av[2];
+	if ((wep.compare("meleeAttack") == 0) || (wep.compare("rangeAttack") == 0)
+		|| (wep.compare("intimidatingShout") == 0))
+	{
+		if (target.compare("") == 0)
+			target = "absolutely nothing";
+
+		std::cout << "This is your input and attack!" << std::endl;
+		attack.action(wep, target);
+		std::cout << std::endl;
+
+		std::cout << "Here are some automated attacks!" << std::endl << std::endl;
+
+		std::cout << "Calling a melee attack!" << std::endl;
+		attack.action("meleeAttack", "the woman who stole your man.");
+		std::cout << std::endl;
+
+		std::cout << "Calling a range attack!" << std::endl;
+		attack.action("rangedAttack", "the man who stole your girl.");
+		std::cout << std::endl;
+
+		std::cout << "Shouting as an attack!" << std::endl;
+		attack.action("intimidatingShout", "the kid who called you ugly.");
+	}
+	else
+	{
+		std::cout << "You must provide a valid attack." << std::endl <<
+		"Valid attacks (case sensitive): meleeAttack, rangedAttack, intimidatingShoot" << std::endl << std::endl <<
+		"Usage: ./a.out [Valid Attack] [Your Target/Victim]" << std::endl;
+		return (0);
+	}
+}
