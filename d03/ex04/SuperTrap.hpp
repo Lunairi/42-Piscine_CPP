8bf95d30e8ebe570ddb3b4317725707983cdc67b
Lunairi <mizukori250@gmail.com>
2018-01-09 19:05:19 -0800

    Finalized d03

--- d03/ex04/SuperTrap.hpp
+++ d03/ex04/SuperTrap.hpp
@@ -23,7 +23,7 @@
 #include "FragTrap.hpp"
 #include "NinjaTrap.hpp"
 
-class	SuperTrap : public FragTrap, public NinjaTrap
+class	SuperTrap : virtual public FragTrap, virtual public NinjaTrap
 {
 
 	public:
