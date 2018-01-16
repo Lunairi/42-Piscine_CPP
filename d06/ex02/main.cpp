cd3b86a6402540b04589928f267c8eee5cfe9bfb
Lunairi <mizukori250@gmail.com>
2018-01-16 14:05:26 -0800

    ??? I think this is right?

---
+++ d06/ex02/main.cpp
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*    main.cpp               _             _              :::      ::::::::   */
+/*    By: mlu               | |           | |           :+:      :+:    :+:   */
+/*     ___  __ _  __ _ _ __ | | __ _ _ __ | |_        +:+ +:+         +:+     */
+/*    / _ \/ _` |/ _` | '_ \| |/ _` | '_ \| __|     +/+  +:+       +/+        */
+/*   |  __/ (_| | (_| | |_) | | (_| | | | | |_    +/+/+/+/+/+   +/+           */
+/*    \___|\__, |\__, | .__/|_|\__,_|_| |_|\__|        /+/    /+/             */
+/*          __/ | __/ | |                             ///   ////////.fr       */
+/*         |___/ |___/|_|                                                     */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+
+Base			*generate(void)
+{
+	int r = rand() % 3;
+
+	if (r == 0)
+		return (new A());
+	else if (r == 1)
+		return (new B());
+	else
+		return (new C());
+}
+
+void			identify_from_pointer(Base *p)
+{
+	if (dynamic_cast<A*>(p))
+		std::cout << "A" << std::endl;
+	else if (dynamic_cast<B*>(p))
+		std::cout << "B" << std::endl;
+	else if (dynamic_cast<C*>(p))
+		std::cout << "C" << std::endl;
+}
+
+void			identify_from_reference(Base &p)
+{
+	if (dynamic_cast<A*>(&p))
+		std::cout << "A" << std::endl;
+	else if (dynamic_cast<B*>(&p))
+		std::cout << "B" << std::endl;
+	else if (dynamic_cast<C*>(&p))
+		std::cout << "C" << std::endl;
+}
+
+int	main()
+{
+	srand(time(0));
+
+	Base	*test = generate();
+
+	identify_from_pointer(test);
+	identify_from_reference(*test);
+
+	return (0);
+}
